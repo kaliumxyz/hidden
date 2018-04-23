071b226496436201201112b6d04925443c05cf6a
kaliumxyz <martijn@becker.wtf>
2018-04-23 11:48:06 +0200

    feat() add more obfuscation tricks and cookies

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const code = [];
 
 // global variables
 let allowed = load('./allowed.json');
+let users = load('./users.json');
 let passwords = load('./passwords.json');
 let posts = load('./posts.json');
 
@@ -40,17 +41,29 @@ function handle() {
 	// I should probably do something here?
 }
 
-function get_posts(posts) {
+function get_posts(posts, access_level = 5) {
 	if(posts)
 		return posts.reduce((res, post) => {
-			if(post.access_level < 5)
-				res + `<h2> ${post.title} </h2> <p> ${post.body} </p> <br>`, '';
-		});
+			if(post.access_level < access_level)
+				return res + `<h2> ${post.title} </h2> <p> ${post.body} </p> <br>`;
+			else 
+				return res;
+		}, '');
 	else
 		return '<p> currently I lack any posts ;-; </p>';
 }
 
-function getIP(req) {
+function add_user(ip = '', access_level = 5) {
+	const id = uuid();
+	// NOTHING CAN STOP ME.
+	if(users[id])
+		return add_user();
+	users[id] = {ip_list: [ip], access_level: access_level};
+	fs.writeFile('./users.json', JSON.stringify(users), handle());
+	return id;
+}
+
+function get_ip(req) {
 	return req.ip || req.connection.address();
 }
 
@@ -60,6 +73,13 @@ function register(ip) {
 	fs.writeFile('./allowed.json', JSON.stringify(allowed), handle());
 }
 
+function encrypt(text) {
+	return Buffer.from(text).toString('base64');
+}
+
+function decrypt(text) {
+	return Buffer.from(text, 'base64').toString();
+}
 // Must configure Raven before doing anything else with it
 //Raven.config(config.raven).install();
 
@@ -69,15 +89,19 @@ app.enable('trust proxy');
 // ws was monkypatched to the app object, see the dependencies
 app.use(cookieParser());
 app.ws('/ws', function(ws, req) {
-	const ip = getIP(req);
-	console.log(req.cookies)
+	const ip = get_ip(req);
+
 	// TODO: send keystrokes instead if efficient
 	ws.on('message', function(ev) {
+		ev = decrypt(ev);
 		if(ev.startsWith('{')) {
 			ev = JSON.parse(ev);
+			if(ev.type === 'devstate') {
+				// some function to ban the person
+			}
+
 			if(ev.type === 'keyup') {
 				let _code = code[req.headers['sec-websocket-key']] || '';
-				console.log(_code);
 				switch(ev.key) {
 				case('Backspace'):
 					_code = _code.slice(0, -1);
@@ -85,32 +109,41 @@ app.ws('/ws', function(ws, req) {
 				case('Enter'):
 					// if our websocket is ready, send the code, if its closed, create a new socket, else ignore this
 					if(passwords[_code]) {
+						let user_add;
+						if(req.cookies && req.cookies.uuid && users[req.cookies.uuid] !== void(1)) { 
+							user_add = '';
+						} else {
+							user_add = `document.cookie = "uuid=${add_user(ip, 5)}";`;
+						}
 						const content = get_posts(posts.posts);
 						if(!allowed[ip])
 							register(ip);
-						ws.send(`
+						ws.send(encrypt(`
 						(() => {
 								i = 100;
 								while(i--)
 									window.history.pushState({"HTML": "there is no going back now", "pageTitle": "There is no going back now"}, '', '/');
 								window.history.pushState({"HTML": "there is no going back now", "pageTitle": "There is no going back now"}, '', '/blog');
-								document.write(\`${!allowed[ip]?'<h1> Thanks for registering':'Hi'} ${req.ip} :D! </h1><hr>${content}\`);
+								document.cookie = "amplitude_id_7acb8df1d57962e98163686ab410fa23_crystal_ce_amplitude=${uuid()}";
+								${user_add}
+								document.write(\`${!allowed[ip]?'<h1> Thanks for registering':'<h1>Hi'} ${req.ip} :D! </h1><hr>${content}\`);
 						})()
-							`);
+							`));
 					}
 					if(_code === 'give me virus') {
 						if(!allowed[ip])
 							register(ip);
-						ws.send(`
+						ws.send(encrypt(`
 						(() => {
 							window.location = '/virus';
 						})()
-							`);
+							`));
 					}
 					if(passwords[_code] && passwords[_code].type === 'one-time'){
 						passwords[_code].active = false;
 						fs.writeFile('./passwords.json', JSON.stringify(passwords), handle());
 					}
+						console.log(_code);
 					_code = '';
 					break;
 				case('Shift'):
@@ -126,22 +159,22 @@ app.ws('/ws', function(ws, req) {
 				code[req.headers['sec-websocket-key']] = _code;
 			}
 		}
-		ws.send('console.log("miss")');
+		// ws.send('console.log("miss")');
 	});
 });
 
 app.use('/virus', (req, res) => {
-	const ip = getIP(req);
+	const ip = get_ip(req);
 	if(allowed[ip])
 		res.sendFile('./virus.exe', {root: './static'});
 });
 
 app.use('/blog', (req, res) => {
-	const ip = getIP(req);
-	if(allowed[ip]) {
+	const ip = get_ip(req);
+	if(req.cookies && req.cookies.uuid && users[req.cookies.uuid] !== void(1)) {
 		const content = get_posts(posts.posts);
 		res.send(`
-<h1> Hi ${req.ip}! </h1>
+<h1> Hi ${ip}! </h1>
 <hr>
 
 ${content}
@@ -153,9 +186,9 @@ ${content}
 
 // Send back our 451
 app.use((req, res) => {
-	const ip = getIP(req);
+	const ip = get_ip(req);
 	let override = false;
-	console.log(req.cookies)
+	console.log(req.cookies.uuid);
 	console.log(ip);
 	if(allowed[ip]) {
 		if(allowed[ip].status === 1){
